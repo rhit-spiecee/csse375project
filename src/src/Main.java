d298b65e0a0605201c8e08af73b6179db27f332e
rhit-smithjk2 <smithjk2@rose-hulman.edu>
2025-03-18 18:28:28 -0400

    Fixing project setup.

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,3 @@
-package src;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,6 +5,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("New line added");
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello world!");
         }
     }
