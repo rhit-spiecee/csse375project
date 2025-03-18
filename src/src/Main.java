4d444a41526b5ab703adfba9c29754357f5ed651
Alex Anisimov <anisima@rose-hulman.edu>
2025-03-18 18:25:03 -0400

    Add a new line to Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("New line added");
         System.out.printf("Hello and welcome!");
         }
     }
