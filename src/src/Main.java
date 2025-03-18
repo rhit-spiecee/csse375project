d995e2492a2e0900b62be3aceace15dba3c9341f
rhit-smithjk2 <smithjk2@rose-hulman.edu>
2025-03-18 18:33:16 -0400

    Jay's First Commit

--- src/src/Main.java
+++ src/src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("New line added");
         System.out.println("Hello world!");
+        System.out.println("Jay's commit");
         }
     }
