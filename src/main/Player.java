d7b209e4ce70adda3ad62890858d9a627f87e184
spiecee <spiecee@rose-hulman.edu>
2025-03-22 15:46:51 -0400

    testDeckContents passes

--- src/main/Player.java
+++ src/main/Player.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
     public int deckSize() {
         return 10;
+    }
+
+    public List<String> getDeck() {
+        ArrayList<String> deck = new ArrayList<String>();
+        for (int i = 0; i < 3; i++) {
+            deck.add("Estate");
+        }
+        for (int i = 0; i < 7; i++) {
+            deck.add("Copper");
+        }
+
+        return deck;
     }
 }
--- src/test/PlayerDeckSetupTests.java
+++ src/test/PlayerDeckSetupTests.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class PlayerDeckSetupTests {
@@ -7,6 +9,12 @@ public class PlayerDeckSetupTests {
     public void testDeckSizeTen(){
         Player player = new Player();
         assertEquals(10, player.deckSize());
+    }
+
+    @Test
+    public void testDeckContents(){
+        Player player = new Player();
+        assertEquals(Arrays.asList("Estate", "Estate", "Estate", "Copper", "Copper", "Copper", "Copper", "Copper", "Copper", "Copper"), player.getDeck());
     }
 
 }
