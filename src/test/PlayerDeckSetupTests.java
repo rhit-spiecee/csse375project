55151ccd4dfe2e69f9d874754e2f1a40c64b1632
spiecee <spiecee@rose-hulman.edu>
2025-03-22 15:53:04 -0400

    testPlayerDeckSizeZero passes

--- src/test/PlayerDeckSetupTests.java
+++ src/test/PlayerDeckSetupTests.java
@@ -5,15 +5,20 @@ import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class PlayerDeckSetupTests {
+    @Test public void testPlayerDeckSizeZero() {
+        Player player = new Player();
+        assertEquals(0, player.deckSize());
+    }
+
     @Test
-    public void testDeckSize(){
+    public void testPlayerDeckSizeTen(){
         Player player = new Player();
         player.createStartingDeck();
         assertEquals(10, player.deckSize());
     }
 
     @Test
-    public void testDeckContents(){
+    public void testPlayerDeckContents(){
         Player player = new Player();
         assertEquals(Arrays.asList("Estate", "Estate", "Estate", "Copper", "Copper", "Copper", "Copper", "Copper", "Copper", "Copper"), player.createStartingDeck());
     }
