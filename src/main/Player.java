83b93d29814da258e717a3966f6c40e258bdf192
spiecee <spiecee@rose-hulman.edu>
2025-03-22 15:40:12 -0400

    testDeckSizeTen passes

---
+++ src/main/Player.java
@@ -0,0 +1,5 @@
+public class Player {
+    public int deckSize() {
+        return 10;
+    }
+}
--- src/test/PlayerDeckSetupTests.java
+++ src/test/PlayerDeckSetupTests.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class PlayerDeckSetupTests {
+    @Test
+    public void testDeckSizeTen(){
+        Player player = new Player();
+        assertEquals(10, player.deckSize());
+    }
+
 }
