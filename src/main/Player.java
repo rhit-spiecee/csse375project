a85ba0a3155679106ed1365c158d007132361095
Alex Anisimov <anisima@rose-hulman.edu>
2025-03-23 14:26:39 -0400

    Start refactoring the Player class
    Co-Authored-By: Ethan Spiece <spiecee@rose-hulman.edu>
    Co-Authored-By: Caleb Lehman <lehmancb@rose-hulman.edu>
    Co-Authored-By: Jay Smith <smithjk2@rose-hulman.edu>

--- src/main/Player.java
+++ src/main/Player.java
@@ -2,18 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    ArrayList<String> deck = new ArrayList<>();
-    public int deckSize() {
-        return deck.size();
-    }
-
-    public List<String> createStartingDeck() {
-        for (int i = 0; i < 3; i++) {
-            deck.add("Estate");
-        }
-        for (int i = 0; i < 7; i++) {
-            deck.add("Copper");
-        }
-        return deck;
-    }
+    Deck deck = new Deck();
+//    public int deckSize() {
+//        return deck.size();
+//    }
+//
+//    public List<String> createStartingDeck() {
+//        for (int i = 0; i < 3; i++) {
+//            deck.add("Estate");
+//        }
+//        for (int i = 0; i < 7; i++) {
+//            deck.add("Copper");
+//        }
+//        return deck;
+//    }
 }
