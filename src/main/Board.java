a472218bc8dac3a4701a6c3d8a48c08f8942db04
spiecee <spiecee@rose-hulman.edu>
2025-03-25 16:07:34 -0400

    testDeckSizeTwoPlayersOnSetup passes
    Co-Authored-By: Alex Anisimov <anisima@rose-hulman.edu>
    Co-Authored-By: Jay Smith <smithjk2@rose-hulman.edu>
    Co-Authored-By: Caleb Lehman <lehmancb@rose-hulman.edu>

--- src/main/Board.java
+++ src/main/Board.java
@@ -1,5 +1,14 @@
 
 public class Board {
+    BoardDeck workshopDeck;
+    BoardDeck woodcutterDeck;
+    BoardDeck villageDeck;
+    BoardDeck smithyDeck;
+    BoardDeck remodelDeck;
+    BoardDeck moatDeck;
+    BoardDeck mineDeck;
+    BoardDeck militiaDeck;
+    BoardDeck marketDeck;
     BoardDeck cellarDeck;
     int numPlayers;
     public Board(int numPlayers) {
@@ -8,6 +17,17 @@ public class Board {
         }
         this.numPlayers = numPlayers;
         this.cellarDeck = new BoardDeck(new Card("cellar", 2, Card.CardType.KINGDOM, 0), 10);
+        this.marketDeck = new BoardDeck(new Card("market", 5, Card.CardType.KINGDOM, 0), 10);
+        this.militiaDeck = new BoardDeck(new Card("militia", 4, Card.CardType.KINGDOM, 0), 10);
+        this.mineDeck = new BoardDeck(new Card("mine", 5, Card.CardType.KINGDOM, 0), 10);
+        this.moatDeck = new BoardDeck(new Card("moat", 2, Card.CardType.KINGDOM, 0), 10);
+        this.remodelDeck = new BoardDeck(new Card("remodel", 4, Card.CardType.KINGDOM, 0), 10);
+        this.smithyDeck = new BoardDeck(new Card("smithy", 4, Card.CardType.KINGDOM, 0), 10);
+        this.villageDeck = new BoardDeck(new Card("village", 3, Card.CardType.KINGDOM, 0), 10);
+        this.workshopDeck = new BoardDeck(new Card("workshop", 3, Card.CardType.KINGDOM, 0), 10);
+        this.woodcutterDeck = new BoardDeck(new Card("woodcutter", 3, Card.CardType.KINGDOM, 0), 10);
+
+
     }
 
 }
--- src/test/BoardSetupTests.java
+++ src/test/BoardSetupTests.java
@@ -26,4 +26,19 @@ public class BoardSetupTests {
         assertThrows(RuntimeException.class, () -> new Board(5));
     }
 
+    @Test
+    public void testDeckSizeTwoPlayersOnSetup(){
+        Board board = new Board(2);
+        assertEquals(10, board.cellarDeck.size());
+        assertEquals(10, board.marketDeck.size());
+        assertEquals(10, board.militiaDeck.size());
+        assertEquals(10, board.mineDeck.size());
+        assertEquals(10, board.moatDeck.size());
+        assertEquals(10, board.remodelDeck.size());
+        assertEquals(10, board.smithyDeck.size());
+        assertEquals(10, board.villageDeck.size());
+        assertEquals(10, board.woodcutterDeck.size());
+        assertEquals(10, board.workshopDeck.size());
+    }
+
 }
