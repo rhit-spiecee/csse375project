37050669682847d35bcaf6656aa875d299803da4
spiecee <spiecee@rose-hulman.edu>
2025-03-25 15:40:55 -0400

    testIllegalSizeOnInit passes
    Co-Authored-By: Alex Anisimov <anisima@rose-hulman.edu>
    Co-Authored-By: Jay Smith <smithjk2@rose-hulman.edu>
    Co-Authored-By: Caleb Lehman <lehmancb@rose-hulman.edu>

--- src/main/BoardDeck.java
+++ src/main/BoardDeck.java
@@ -4,6 +4,9 @@ public class BoardDeck {
     private final Stack<Card> deck = new Stack<>();
 
     public BoardDeck(Card card, int size) {
+        if(size < 8){
+            throw new RuntimeException("Size must be at least 8");
+        }
         for(int i = 0; i < size; i++) {
             deck.add(card);
         }
--- src/test/BoardDeckTests.java
+++ src/test/BoardDeckTests.java
@@ -5,7 +5,14 @@ import static org.junit.Assert.*;
 public class BoardDeckTests {
     @Test
     public void testKingdomDeckSizeOnInit() {
-        BoardDeck cellarDeck = new BoardDeck(new Card("cellar", 2, Card.CardType.KINGDOM, 0), 10);
-        assertEquals(10, cellarDeck.size());
+        BoardDeck cellarDeck = new BoardDeck(new Card("cellar", 2, Card.CardType.KINGDOM, 0), 8);
+        assertEquals(8, cellarDeck.size());
+    }
+
+    @Test
+    public void testIllegalSizeOnInit() {
+        assertThrows(RuntimeException.class, () -> {
+            new BoardDeck(new Card("cellar", 2, Card.CardType.KINGDOM, 0), 7);
+        });
     }
 }
