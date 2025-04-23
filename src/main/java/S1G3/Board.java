8354d3b20b812c28f1c3a90e4b4d058188e1eb85
spiecee <spiecee@rose-hulman.edu>
2025-04-23 14:10:43 -0400

    testProvinceDeckEmpty passes

--- src/main/java/S1G3/Board.java
+++ src/main/java/S1G3/Board.java
@@ -82,6 +82,13 @@ public class Board {
     public void startGame() {
         while (!gameOver) {
             processTurn();
+            checkProvinceDeckLength();
+        }
+    }
+
+    public void checkProvinceDeckLength() {
+        if(victoryDecks.get("province").size() <= 0) {
+            gameOver = true;
         }
     }
 
