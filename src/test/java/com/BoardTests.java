5fc0b50f4bcf252defe781e0788e51acb652c707
spiecee <spiecee@rose-hulman.edu>
2025-05-07 15:11:06 -0400

    testStartGameAndGameOver passes

--- src/test/java/com/BoardTests.java
+++ src/test/java/com/BoardTests.java
@@ -88,38 +88,41 @@ public class BoardTests {
         assertFalse(board.gameOver);
     }
     
-//    @Test
-//    public void testStartGameAndGameOver() {
-//        Gui mockGui = EasyMock.mock(Gui.class);
-//        Player player1 = EasyMock.mock(Player.class);
-//        Player player2 = EasyMock.mock(Player.class);
-//        BoardDeck mockDeck = EasyMock.mock(BoardDeck.class);
-//
-//        EasyMock.expect(player1.calculateScore()).andReturn(18);
-//        EasyMock.expect(player1.getHand()).andReturn(new ArrayList<>());
-//        EasyMock.expect(player1.getCoins()).andReturn(0);
-//        EasyMock.expect(player1.getActions()).andReturn(1);
-//        EasyMock.expect(player1.getBuys()).andReturn(1);
-//
-//        EasyMock.expect(player2.calculateScore()).andReturn(25);
-//
-//        EasyMock.expect(mockDeck.size()).andReturn(0).anyTimes();
-//
-//        EasyMock.expect(mockGui.getNumPlayers()).andReturn(2);
-//        EasyMock.expect(mockGui.getActionSelection(0)).andReturn(1);
-//        mockGui.updateView(EasyMock.isA(BoardDto.class));
-//        mockGui.displayGameOverScreen(EasyMock.anyObject());
-//
-//        EasyMock.replay(mockGui, player1, player2, mockDeck);
-//
-//        Board board = Board.fromGui(mockGui);
-//        board.players = Arrays.asList(player1, player2);
-//        board.victoryDecks.put("province" , mockDeck);
-//
-//        board.startGame();
-//
-//        EasyMock.verify(mockGui, player1, player2, mockDeck);
-//    }
+    @Test
+    public void testStartGameAndGameOver() {
+        Gui mockGui = EasyMock.mock(Gui.class);
+        Player player1 = EasyMock.mock(Player.class);
+        Player player2 = EasyMock.mock(Player.class);
+        BoardDeck mockDeck = EasyMock.mock(BoardDeck.class);
+
+        EasyMock.expect(player1.calculateScore()).andReturn(18);
+        EasyMock.expect(player1.getHand()).andReturn(new ArrayList<>());
+        EasyMock.expect(player1.getCoins()).andReturn(0);
+        EasyMock.expect(player1.getActions()).andReturn(1);
+        EasyMock.expect(player1.getBuys()).andReturn(1);
+
+        EasyMock.expect(player2.calculateScore()).andReturn(25);
+
+        EasyMock.expect(mockDeck.size()).andReturn(0).anyTimes();
+
+        EasyMock.expect(mockGui.getNumPlayers()).andReturn(2);
+        EasyMock.expect(mockGui.getActionSelection(0)).andReturn(1);
+        mockGui.updateView(EasyMock.isA(BoardDto.class));
+        mockGui.displayGameOverScreen(EasyMock.anyObject());
+
+        EasyMock.replay(mockGui, player1, player2, mockDeck);
+
+        Board board = Board.fromGui(mockGui);
+        board.players = Arrays.asList(player1, player2);
+        board.victoryDecks.put("province" , mockDeck);
+
+        assertFalse(board.gameOver);
+
+        board.startGame();
+
+        assertTrue(board.gameOver);
+        EasyMock.verify(mockGui, player1, player2, mockDeck);
+    }
     
     @Test
     public void testGetSortedPlayerEntries() {
@@ -203,30 +206,7 @@ public class BoardTests {
 
         EasyMock.verify(mockGui, player1);
     }
-    
-//    @Test
-//    public void testActionPhaseNoActionCardsAvailable() {
-//        Gui mockGui = EasyMock.mock(Gui.class);
-//
-//        Player player1 = EasyMock.mock(Player.class);
-//
-//        EasyMock.expect(player1.getActions()).andReturn(1);
-//        EasyMock.expect(player1.hasActionCardInHand()).andReturn(false);
-//
-//        EasyMock.expect(mockGui.getNumPlayers()).andReturn(2);
-//        EasyMock.expect(mockGui.getActionSelection(0)).andReturn(0);
-//        mockGui.showErrorPopup("Player 1 has no action cards");
-//
-//        EasyMock.replay(mockGui, player1);
-//
-//        Board board = Board.fromGui(mockGui);
-//        board.players.removeFirst();
-//        board.players.addFirst(player1);
-//
-//        board.actionPhase();
-//
-//        EasyMock.verify(mockGui, player1);
-//    }
+
     
     @Test
     public void testGetCardByNameEmptyCardList() {
